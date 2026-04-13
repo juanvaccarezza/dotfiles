3feb0d194535a4d443dc46cfdf5adad32d769d55
Juan Ignacio Vaccarezza <jivaccarezza@invap.com.ar>
2026-04-13 15:01:36 -0300

    Add rust analyzxer allTargets =false

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -2,3 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+vim.g.rustaceanvim = {
+  server = {
+    settings = {
+      ["rust-analyzer"] = {
+        check = {
+          allTargets = false,
+        },
+        lru = {
+          capacity = 32,
+        },
+      },
+    },
+  },
+}
